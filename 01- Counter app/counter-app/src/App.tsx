ec79e26fe349309b7c84ede1ea7e858fcdcffb09
Lucas Augusto Mendes Rodrigues <lucas.augustus@gmail.com>
2021-09-19 18:57:55 -0300

    Made the style

--- 01- Counter app/counter-app/src/App.tsx
+++ 01- Counter app/counter-app/src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={SBackground}>
+      <button style={{ ...SButton, marginRight: "1rem" }} onClick={decrement}>
+        <span style={{ ...SButtonText }}>-</span>
+      </button>
+      <div className="number-show" style={SCounterDisplay}>
+        <span
+          className="text-number"
+          style={{ ...SNumberText, userSelect: "none", fontWeight: "bold" }}
         >
-          Learn React
-        </a>
-      </header>
+          {count}
+        </span>
+      </div>
+      <button style={{ ...SButton, marginLeft: "1rem" }} onClick={increment}>
+        <span style={SButtonText}>+</span>
+      </button>
     </div>
   );
 }
 
 export default App;
+
+const SBackground = {
+  backgroundColor: "#D6E8FC",
+  display: "flex",
+  height: "100vh",
+  width: "100vw",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const SCounterDisplay = {
+  backgroundColor: "#0F73E3",
+  height: "5rem",
+  width: "5rem",
+  borderRadius: "0.8rem",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const SNumberText = {
+  color: "#FFF",
+  fontSize: "2rem",
+};
+
+const SButton = {
+  cursor: "pointer",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "transparent",
+  border: "none",
+  fontSize: "2rem",
+  height: "2rem",
+  width: "2rem",
+  color: "#2F81E2",
+};
+
+const SButtonText = {
+  height: "2rem",
+  display: "flex",
+  alignItems: "center",
+};
